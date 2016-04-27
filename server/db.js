fc0dabeb79d8c00785c023e0b7cccb907bb935d1
Chris Staton <chris@plastiq.com>
2016-04-26 17:18:10 -0700

    setup basic server

--- server/db.js
+++ server/db.js
@@ -1,17 +1,17 @@
-var Sequelize = require('sequelize');
+// var Sequelize = require('sequelize');
 
-var orm = new Sequelize('myTest', 'root', null, {
-  host: 'localhost',
-  dialect: 'mysql',
-  define: {
-    timestamps: true
-  }
-});
+// var orm = new Sequelize('myTest', 'root', null, {
+//   host: 'localhost',
+//   dialect: 'mysql',
+//   define: {
+//     timestamps: true
+//   }
+// });
 
-var Users = orm.define('Users', {
-  mixpanelId: Sequelize.STRING,
-  firstName: Sequelize.STRING,
-  lastName: Sequelize.STRING
-});
+// var Users = orm.define('Users', {
+//   mixpanelId: Sequelize.STRING,
+//   firstName: Sequelize.STRING,
+//   lastName: Sequelize.STRING
+// });
 
-module.exports = Users;
+// module.exports = Users;
--- server/myServer.js
+++ server/myServer.js
@@ -1,23 +1,34 @@
-var MixpanelExport = require('mixpanel-data-export');
-var Async = require('async');
-var db = require('./db');
+// var MixpanelExport = require('mixpanel-data-export');
+// var Async = require('async');
+// var db = require('./db');
 
 
-var panel = new MixpanelExport({
-  api_key: "bab29e942121cfebdb84b85bda728c14",
-  api_secret: "70fbdcee8c70e3f546e84c44f60cea5c"
-});
+// var panel = new MixpanelExport({
+//   api_key: "bab29e942121cfebdb84b85bda728c14",
+//   api_secret: "70fbdcee8c70e3f546e84c44f60cea5c"
+// });
 
 
-panel
-  .engage({where: 'properties["$last_seen"]>="2016-04-21T14:25:16"'})
-  .then(function(results) {
-    console.log('***** Results ******');
-    Async.each(results.results, function(item) {
-      db.create({
-        mixpanelId: item.$distinct_id,
-        firstName: item.$properties.$first_name,
-        lastName: item.$properties.$last_name
-      });
-    });
-  });
+// panel
+//   .engage({where: 'properties["$last_seen"]>="2016-04-21T14:25:16"'})
+//   .then(function(results) {
+//     console.log('***** Results ******');
+//     Async.each(results.results, function(item) {
+//       db.create({
+//         mixpanelId: item.$distinct_id,
+//         firstName: item.$properties.$first_name,
+//         lastName: item.$properties.$last_name
+//       });
+//     });
+//   });
+
+var express = require('express');
+var app = express();
+
+app.use(express.static(__dirname + "/../client"));
+
+app.listen(80);
+
+
+
+
