56e91c3564263509fad879ad3b7891c032fd8ad3
Chris Staton <chris@plastiq.com>
2016-04-26 17:09:32 -0700

    setup repo

---
+++ server/db.js
@@ -0,0 +1,17 @@
+var Sequelize = require('sequelize');
+
+var orm = new Sequelize('myTest', 'root', null, {
+  host: 'localhost',
+  dialect: 'mysql',
+  define: {
+    timestamps: true
+  }
+});
+
+var Users = orm.define('Users', {
+  mixpanelId: Sequelize.STRING,
+  firstName: Sequelize.STRING,
+  lastName: Sequelize.STRING
+});
+
+module.exports = Users;
